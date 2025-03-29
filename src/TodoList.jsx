4480a33108c50f3b01575af13737e3e82f91f921
AnnaBazileeva <anna.bazileeva@gmail.com>
2025-03-29 12:00:58 -0400

    add useState

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,3 +1,4 @@
+import TodoListItem from "./TodoListItem.jsx";
 function TodoList() {
     const todos = [
         {id: 1, title: "review resources"},
@@ -7,7 +8,10 @@ function TodoList() {
 
     return (
         <div>
-            <ul>{todos.map(todo => <li key={todo.id}>{todo.title}</li>)}</ul>
+            <ul>
+                {todos.map(todo =>(
+                <TodoListItem key={todo.id} todo={todo} />))}
+            </ul>
         </div>
     );
 }
