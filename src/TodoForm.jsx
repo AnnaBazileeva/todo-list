163ed11ad884f0bb3134aeb9673a8a1428fb0ab0
AnnaBazileeva <anna.bazileeva@gmail.com>
2025-04-10 23:19:37 -0400

    event.target.title

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,12 +1,15 @@
 function TodoForm({onAddTodo}) {
     const handleAddTodo = (event) => {
         event.preventDefault();
-        console.dir(event.target)
+        console.dir(event.target.title)
     }
 
     return (
         <form onSubmit={handleAddTodo}>
             <input type='text' name='title' placeholder='enter todo'/>
+            <button type='submit'>Add Todo</button>
         </form>
     )
 }
+
+export default TodoForm
