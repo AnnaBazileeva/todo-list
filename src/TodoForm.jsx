ac5091a65149924dc93f5c9f95269ee51dae61f3
AnnaBazileeva <anna.bazileeva@gmail.com>
2025-04-10 23:45:11 -0400

    add todolist, todolistitem

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,12 +1,18 @@
+import {useRef} from "react";
+
 function TodoForm({onAddTodo}) {
+    const todoTitleInput =useRef('')
     const handleAddTodo = (event) => {
         event.preventDefault();
-        console.dir(event.target.title)
+      const title = event.target.title.value;
+      onAddTodo(title);
+      event.target.title.value = '';
+      todoTitleInput.current.focus()
     }
 
     return (
         <form onSubmit={handleAddTodo}>
-            <input type='text' name='title' placeholder='enter todo'/>
+            <input type='text' name='title' placeholder='enter todo' ref={todoTitleInput}/>
             <button type='submit'>Add Todo</button>
         </form>
     )
