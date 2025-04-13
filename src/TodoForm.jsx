3397b7491027403c8f7b97681b8a590f5ea00e67
AnnaBazileeva <anna.bazileeva@gmail.com>
2025-04-13 00:27:15 -0400

    todoform changed using workingtodo

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,18 +1,21 @@
-import {useRef} from "react";
+import {useRef, useState} from "react";
+
 
 function TodoForm({onAddTodo}) {
+    const [workingTodo,setWorkingTodo] = useState('')
     const todoTitleInput =useRef('')
+
     const handleAddTodo = (event) => {
         event.preventDefault();
-      const title = event.target.title.value;
-      onAddTodo(title);
-      event.target.title.value = '';
+      const newTodo = workingTodo;
+      onAddTodo(newTodo);
+      setWorkingTodo('');
       todoTitleInput.current.focus()
     }
 
     return (
         <form onSubmit={handleAddTodo}>
-            <input type='text' name='title' placeholder='enter todo' ref={todoTitleInput}/>
+            <input type='text' name='title' placeholder='enter todo' ref={todoTitleInput} value={workingTodo}/>
             <button type='submit'>Add Todo</button>
         </form>
     )
