024ed7895024ce0a5bf5392ed9b4720d430ba978
AnnaBazileeva <anna.bazileeva@gmail.com>
2025-05-22 15:24:04 -0400

    use styled-compomemts

--- src/TodoList/TodoListItem.jsx
+++ src/TodoList/TodoListItem.jsx
@@ -1,5 +1,30 @@
 import { useState, useEffect } from "react";
+import styled from 'styled-components'
 import TextInputWithLabel from "../shared/TextInputWithLabel.jsx";
+
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1rem;`
+
+const StyledButton = styled.button`
+  margin-right: 0.5rem;
+  padding: 0.25rem 0.5rem;
+  font-style: ${({ disabled }) => (disabled ? "italic" : "normal")};`
+
+const StyledItemWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  margin-bottom: 1rem;`
+
+const StyledCheckbox = styled.input`
+  margin-right: 0.5rem;`
+
+const StyledTitle = styled.span`
+  flex: 1;`
 
     function TodoListItem({ todo, onUpdateTodo, onDeleteTodo }) {
         const [isEditing, setIsEditing] = useState(false);
@@ -39,38 +64,33 @@ import TextInputWithLabel from "../shared/TextInputWithLabel.jsx";
         return (
             <div>
                 {isEditing ? (
-                    <form onSubmit={handleUpdate}>
+                    <StyledForm onSubmit={handleUpdate}>
                         <TextInputWithLabel
                             elementId={`edit-${todo.id}`}
                             label="Edit Todo"
                             value={workingTitle}
                             onChange={handleEdit}
                         />
-                        <button type="submit" >
+                        <StyledButton type="submit" >
                             Update
-                        </button>
-                        <button type="button" onClick={handleCancel}>
+                        </StyledButton>
+                        <StyledButton type="button" onClick={handleCancel}>
                             Cancel
-                        </button><button  type="button" onClick={handleDelete}>Delete</button>
-                    </form>
+                        </StyledButton>
+                        <StyledButton  type="button" onClick={handleDelete}>Delete</StyledButton>
+                    </StyledForm>
                 ) : (
-                    <>
-                    <div>
-                        <input
+                    <StyledItemWrapper>
+                        <StyledCheckbox
                             type="checkbox"
                             checked={todo.isCompleted}
                             onChange={handleToggle}
                         />
-                        <span>{todo.title}</span>
-                        <button onClick={() => setIsEditing(true)}>Edit</button>
-                        <button onClick={handleDelete}>Delete</button>
-                    </div>
-
-
-
-                    </>
-                )
-                }
+                        <StyledTitle>{todo.title}</StyledTitle>
+                        <StyledButton onClick={() => setIsEditing(true)}>Edit</StyledButton>
+                        <StyledButton onClick={handleDelete}>Delete</StyledButton>
+                    </StyledItemWrapper>
+                )}
             </div>
         )}
 export default TodoListItem;
